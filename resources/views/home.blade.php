cbc082c191b8ed0770f6d8eb7999ac3d26c3e161
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-05-23 16:42:01 +0200

    change div class row in ul class row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
 
 @section('name')
     <main>
-        <div class="container">
-            <div class="row gy-4">
+        <div class="container" style="height: 100vh" style="overflow: scroll">
+            <ul class="row gy-4 list-unstyled">
                 @foreach ($movies as $movie)
                     
                 
-                <div class="col-3">
+                <li class="col-3 flex-grow-1">
                     <div class="card h-100 d-flex align-items-center">
                         <img class="w-100 h-75" src="{{$image[($movie->id)-1]}}" alt="">
                         <h4>{{$movie->title}}</h4>
@@ -17,9 +17,9 @@
                         <p>{{$movie->date}}</p>
                         <p>{{$movie->vote}}</p>
                     </div>
-                </div>
+                </li>
                 @endforeach
-            </div>
+            </ul>
         </div>
     </main>
 @endsection
